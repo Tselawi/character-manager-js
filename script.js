8844308e8de185726e72f2a24ea5523a33a9077b
Tomi <szobonyatamas@gmail.com>
2021-03-05 14:29:09 +0100

    djoijowkjf

--- script.js
+++ script.js
@@ -10,11 +10,13 @@
 
   heroes.forEach(({ name, shortDescription, image, id }) => {
     const elt = tpl.cloneNode(true).content;
-    //console.log(id);
+    console.log(id);
 
     elt.querySelector('.charName').innerText = name;
 
     elt.querySelector('.charShortDesc').innerText = shortDescription;
+
+    elt.querySelector('.charId').innerText = id;
 
     if (image === undefined) {
       elt
@@ -28,19 +30,7 @@
     const butMore = document.createElement('button');
     const butEdit = document.createElement('button');
     const butDel = document.createElement('button');
-    // const textMore = '';
-    // const textEdit = '';
-    // const textDel = '';
 
-    // let z = document.createTextNode(textMore);
-    // let a = document.createTextNode(textEdit);
-    // let b = document.createTextNode(textDel);
-
-    //document.setAttribute('src', './src/assets/menu.svg');
-
-    // butMore.appendChild(z);
-    // butEdit.appendChild(a);
-    // butDel.appendChild(b);
     elt
       .querySelector('.actions')
       .appendChild(butMore)
@@ -59,102 +49,138 @@
     target.appendChild(elt);
     const modalBg = document.querySelector('.modal-bg');
     const modalClose = document.querySelector('.modal-close');
-    (function modal() {
-      // for the botton more
-      Array.from(document.querySelectorAll('.moreBtn')).forEach(
-        (btn, index) => {
-          btn.addEventListener('click', function () {
-            modalBg.classList.add('modal-active');
-          });
-          modalClose.addEventListener('click', function () {
-            modalBg.classList.remove('modal-active');
-          });
-        }
-      );
-      //for edit botton edit JSON infon in card
-      const modalNewBg = document.querySelector('.modalNew-bg');
-      Array.from(document.querySelectorAll('.editBtn')).forEach(
-        (btn, index) => {
-          const inputs = Array.from(document.querySelectorAll('input'));
-          btn.addEventListener('click', async () => {
-            modalNewBg.classList.add('modal-active');
-            const values = inputs.map(({ value }) => value.trim());
-            document
-              .querySelector('.nameEditInput')
-              .setAttribute('placeholder', 'newname');
-            document.querySelector('#run').setAttribute('class', 'editRun');
-            const [name, shortDescription] = values;
 
-            const response = await fetch(
-              `https://character-database.becode.xyz/characters`,
-              {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  name,
-                  shortDescription,
-                  //  image,
-                  //description,
-                }),
-              }
-            );
+    // for the botton more
 
-            const freshHero = await response.json();
-          });
+    // (function modal() {
+    //   Array.from(document.querySelectorAll('.moreBtn')).forEach(
+    //     (btn, index) => {
+    //       btn.addEventListener('click', function () {
+    //         modalBg.classList.add('modal-active');
+    //       });
+    //       modalClose.addEventListener('click', function () {
+    //         modalBg.classList.remove('modal-active');
+    //       });
 
-          modalClose.addEventListener('click', () => {
-            modalBg.classList.remove('modal-active');
-          });
-        }
-      );
+    //     }
+    //   );
 
-      // for delete botton & delete JSON INFO from the card
-      Array.from(document.querySelectorAll('.delBtn')).forEach((btn) => {
-        btn.addEventListener('click', async () => {
-          modalBg.classList.add('modal-active');
-          console.log({ id });
+    const charCard = document.querySelector('.charCard'); //charCard
+    const modal = document.querySelector('.charModal'); //modal
+    const input = document.querySelector('.charId'); //char id
 
-          try {
-            const response = await fetch(
-              `https://character-database.becode.xyz/characters/${id}`,
-              {
-                method: 'DELETE',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              }
-            );
+    document.querySelector('.moreBtn').addEventListener('click', async () => {
+      modalBg.classList.add('modal-active');
+      const id = input.innerHTML;
+      console.log(id);
 
-            const deleteditem = await response.json();
+      // if (isNaN(id) || id === 0) {
+      //   console.error('Invalid id!');
+      //   //return;
+      // }
 
-            console.log(deleteditem);
-          } catch (err) {
-            console.error(`Unknown item with id: ${id}!`);
+      try {
+        const response = await fetch(
+          `https://character-database.becode.xyz/characters/${id}`
+        );
+        const { name, description, abilities } = await response.json();
+
+        const elt = charCard.cloneNode(true).content;
+
+        elt.querySelector('.name').innerText = name;
+        elt.querySelector('.charDesc').innerText = description;
+        //elt.querySelector(".powers").innerText = abilities.join(", ");
+
+        modal.firstElementChild
+          ? modal.replaceChild(elt, modal.firstElementChild)
+          : modal.appendChild(elt);
+      } catch (err) {
+        console.error(`Unknown hero with id: ${id}!`);
+      }
+    });
+
+    //for edit botton edit JSON infon in card
+    const modalNewBg = document.querySelector('.modalNew-bg');
+    Array.from(document.querySelectorAll('.editBtn')).forEach((btn, index) => {
+      const inputs = Array.from(document.querySelectorAll('input'));
+      btn.addEventListener('click', async () => {
+        modalNewBg.classList.add('modal-active');
+        const values = inputs.map(({ value }) => value.trim());
+        document
+          .querySelector('.nameEditInput')
+          .setAttribute('placeholder', 'newname');
+        document.querySelector('#run').setAttribute('class', 'editRun');
+        const [name, shortDescription] = values;
+
+        const response = await fetch(
+          `https://character-database.becode.xyz/characters`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              name,
+              shortDescription,
+              //  image,
+              //description,
+            }),
           }
-        });
-        modalClose.addEventListener('click', () => {
-          modalBg.classList.remove('modal-active');
-        });
+        );
+
+        const freshHero = await response.json();
       });
-    })();
+
+      modalClose.addEventListener('click', () => {
+        modalBg.classList.remove('modal-active');
+      });
+    });
+
+    // for delete botton & delete JSON INFO from the card
+    Array.from(document.querySelectorAll('.delBtn')).forEach((btn) => {
+      btn.addEventListener('click', async () => {
+        modalBg.classList.add('modal-active');
+        console.log({ id });
+
+        try {
+          const response = await fetch(
+            `https://character-database.becode.xyz/characters/${id}`,
+            {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+
+          const deleteditem = await response.json();
+
+          console.log(deleteditem);
+        } catch (err) {
+          console.error(`Unknown item with id: ${id}!`);
+        }
+      });
+      modalClose.addEventListener('click', () => {
+        modalBg.classList.remove('modal-active');
+      });
+    });
+    //})();
   });
 })();
 
 // new button&modal//
 
-(function modalNew() {
-  const modalNewBg = document.querySelector('.modalNew-bg');
-  const modalNewClose = document.querySelector('.modalNew-close');
+// (function modalNew() {
+//   const modalNewBg = document.querySelector('.modalNew-bg');
+//   const modalNewClose = document.querySelector('.modalNew-close');
 
-  document.querySelector('.newBtn').addEventListener('click', function () {
-    modalNewBg.classList.add('modal-active');
-  });
-  modalNewClose.addEventListener('click', () => {
-    modalNewBg.classList.remove('modal-active');
-  });
-})();
+//   document.querySelector('.newBtn').addEventListener('click', function () {
+//     modalNewBg.classList.add('modal-active');
+//   });
+//   modalNewClose.addEventListener('click', () => {
+//     modalNewBg.classList.remove('modal-active');
+//   });
+// })();
 
 // to open load the image
 
