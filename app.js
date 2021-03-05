f12c44a77102b32f1221aaa0df52b8598830fa49
Tariq <selawi.be@gmail.com>
2021-03-05 14:22:35 +0100

    new app

---
+++ app.js
@@ -0,0 +1,155 @@
+const postsList = document.querySelector('.post-list');
+const addPostForm = document.querySelector('.add-post-form');
+const nameValue = document.getElementById('name-value');
+const shortValue = document.getElementById('short-value');
+const imgValue = document.getElementById('image-value');
+const descValue = document.getElementById('body-value');
+const btnSubmit = document.querySelector('.btn');
+
+let outPut= '';
+
+const renderPosts = (posts)=>{
+    posts.forEach(post => {
+        //console.log(post)
+        outPut +=`
+        <div class= card mt-4 col-md-6 >
+        <div class="card-body border border-secondary">
+        <div class="row">
+        <div class="col-md-4 border border-secondary image-card">
+        <img src="..." alt="...">
+        </div>
+        
+        <div class="col-md-6 mx-5">
+            <div class="card-body" data-id=${post.id}>
+            <h5 class="card-title">${post.name}</h5>
+            <p class="card-text">${post.shortDescription}</p>
+            <button class="btn btn-primary" id="moreBtn">More</button>
+            <button class="btn btn-primary" id="editBtn">Edit</button>
+            <button class="btn btn-primary" id="deleteBtn">Delete</button>
+            </div>
+            </div>
+        </div>
+        </div
+        `;
+    });
+    postsList.innerHTML=outPut;
+}
+const url ="https://character-database.becode.xyz/characters";
+
+// Get read the posts
+// Method:Get
+fetch(url)
+    .then(response => response.json())
+    .then(data => renderPosts(data));
+
+
+    postsList.addEventListener('click', (event)=>{
+       // event.preventDefault();
+        //console.log(event.target.id)
+        let delBtnPressed = event.target.id == "deleteBtn";
+        let editBtnPressed = event.target.id == "editBtn";
+        let moteBtnPressed = event.target.id == "moreBtn";
+
+        let idData=event.target.parentElement.dataset.id;
+        //Delete - remove existing post
+        // method: DELETE
+        if(delBtnPressed){
+            //console.log('remove post')
+            fetch(`${url}/${idData}`,{
+                method:'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            })
+            .then(response => response.json())
+            .then(()=> location.reload())//refresh the window
+        }
+
+        //Edit - change existing post
+        // method: GET
+        if(editBtnPressed){
+            //console.log('edit post')
+            const cardData = event.target.parentElement;
+            let nameContent = cardData.querySelector(".card-title").textContent;
+            let shortContent = cardData.querySelector(".card-text").textContent;
+            //let imageContent = cardData.querySelector(".image-card").textContent;
+            //console.log(nameContent)
+            //console.log(shortContent)
+            //console.log(imageContent)
+            nameValue.value = nameContent;
+            shortValue.value = shortContent;
+            //imgValue.value = shortContent
+
+            // update - update the existing post
+            //method: PATCH
+            btnSubmit.addEventListener('click', ()=>{
+                // console.log('post update!')
+                //event.preventDefault(); //to not repeat the submit
+                fetch(`${url}/${idData}`, {
+                    method:'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name:nameValue.value,
+                        shortDescription:shortValue.value,
+                        //image:imgValue.value,
+                    })
+                })
+                .then(response => response.json())
+                .then(()=> location.reload())
+            })
+
+        }
+
+        //Details - Show existing post
+        // method: GET
+
+        if(moteBtnPressed){
+            //console.log('more post')
+            const cardData = event.target.parentElement;
+            let nameContent = cardData.querySelector(".card-title").textContent;
+            let shortContent = cardData.querySelector(".card-text").textContent;
+            //let imageContent = cardData.querySelector(".image-card").textContent;
+            console.log(nameContent)
+            console.log(shortContent)
+            //console.log(imageContent)
+            nameValue.value = nameContent;
+            shortValue.value = shortContent;
+            //imgValue.value = shortContent
+        }
+
+    });
+
+
+// create - insert new post
+// method: POST
+addPostForm.addEventListener('submit', ()=>{
+    //e.preventDefault();
+    //console.log(nameValue.value);
+    fetch(url, {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name:nameValue.value,
+            shortDescription:shortValue.value,
+            //image:imgValue.value,
+            description:descValue.value
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const dataArr = [];
+        dataArr.push(data);
+        renderPosts(dataArr);
+    })
+
+    // reset input field to empty after submit
+
+    nameValue.value= ""; 
+    shortValue.value= "";
+    //image:imgValue.value= "";
+    descValue.value="";
+});
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Create list from JSON and make grid //
+
+///// Start the code /////
 (async function getData() {
   const tpl = document.querySelector('#charCard');
   const target = document.querySelector('#target');
@@ -8,9 +10,9 @@
   );
   const heroes = await response.json();
 
-  heroes.forEach(({ name, shortDescription, image, id }) => {
+  heroes.forEach(({ name, shortDescription, image, id}) => {
     const elt = tpl.cloneNode(true).content;
-    //console.log(id);
+   // console.log(id);
 
     elt.querySelector('.charName').innerText = name;
 
@@ -25,22 +27,11 @@
         .querySelector('.charImg')
         .setAttribute('src', 'data:image/png;base64,' + image);
     }
+ 
     const butMore = document.createElement('button');
     const butEdit = document.createElement('button');
     const butDel = document.createElement('button');
-    // const textMore = '';
-    // const textEdit = '';
-    // const textDel = '';
 
-    // let z = document.createTextNode(textMore);
-    // let a = document.createTextNode(textEdit);
-    // let b = document.createTextNode(textDel);
-
-    //document.setAttribute('src', './src/assets/menu.svg');
-
-    // butMore.appendChild(z);
-    // butEdit.appendChild(a);
-    // butDel.appendChild(b);
     elt
       .querySelector('.actions')
       .appendChild(butMore)
@@ -49,7 +40,7 @@
     elt
       .querySelector('.actions')
       .appendChild(butEdit)
-      .setAttribute('class', 'editBtn btn');
+      .setAttribute('class', 'editBtn btn', 'method', 'GET');
 
     elt
       .querySelector('.actions')
@@ -57,65 +48,15 @@
       .setAttribute('class', 'delBtn btn', 'method', 'Delete');
 
     target.appendChild(elt);
-    const modalBg = document.querySelector('.modal-bg');
-    const modalClose = document.querySelector('.modal-close');
-    (function modal() {
-      // for the botton more
-      Array.from(document.querySelectorAll('.moreBtn')).forEach(
-        (btn, index) => {
-          btn.addEventListener('click', function () {
-            modalBg.classList.add('modal-active');
-          });
-          modalClose.addEventListener('click', function () {
-            modalBg.classList.remove('modal-active');
-          });
-        }
-      );
-      //for edit botton edit JSON infon in card
-      const modalNewBg = document.querySelector('.modalNew-bg');
-      Array.from(document.querySelectorAll('.editBtn')).forEach(
-        (btn, index) => {
-          const inputs = Array.from(document.querySelectorAll('input'));
-          btn.addEventListener('click', async () => {
-            modalNewBg.classList.add('modal-active');
-            const values = inputs.map(({ value }) => value.trim());
-            document
-              .querySelector('.nameEditInput')
-              .setAttribute('placeholder', 'newname');
-            document.querySelector('#run').setAttribute('class', 'editRun');
-            const [name, shortDescription] = values;
 
-            const response = await fetch(
-              `https://character-database.becode.xyz/characters`,
-              {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  name,
-                  shortDescription,
-                  //  image,
-                  //description,
-                }),
-              }
-            );
-
-            const freshHero = await response.json();
-          });
-
-          modalClose.addEventListener('click', () => {
-            modalBg.classList.remove('modal-active');
-          });
-        }
-      );
-
-      // for delete botton & delete JSON INFO from the card
-      Array.from(document.querySelectorAll('.delBtn')).forEach((btn) => {
+       // for delete botton & delete JSON INFO from the card
+      
+        document.querySelectorAll('.delBtn').forEach(btn =>{
         btn.addEventListener('click', async () => {
-          modalBg.classList.add('modal-active');
-          console.log({ id });
-
+          //modalBg.classList.add('modal-active');
+          
+          
+          
           try {
             const response = await fetch(
               `https://character-database.becode.xyz/characters/${id}`,
@@ -131,16 +72,93 @@
 
             console.log(deleteditem);
           } catch (err) {
-            console.error(`Unknown item with id: ${id}!`);
+            console.error(`Unknown item with id:${id}`);
           }
-        });
-        modalClose.addEventListener('click', () => {
-          modalBg.classList.remove('modal-active');
-        });
+        
       });
-    })();
+        //modalClose.addEventListener('click', () => {
+          //modalBg.classList.remove('modal-active');
+      
+    });
   });
-})();
+  
+  ///// end the code /////
+
+  ////////////////////////////////
+///// Start the code /////
+    const modalBg = document.querySelector('.modal-bg');
+    const modalClose = document.querySelector('.modal-close');
+    (function modal() {
+      // for the botton more
+      Array.from(document.querySelectorAll('.moreBtn')).forEach(
+        (btn) => {
+          btn.addEventListener('click', function () {
+            modalBg.classList.add('modal-active');
+          });
+          modalClose.addEventListener('click', function () {
+            modalBg.classList.remove('modal-active');
+          });
+        }
+      );
+    })();
+///// end the code /////
+////////////////////////
+
+///////start the code ///////
+      //for edit botton edit JSON infon in card
+      const modalNewBg = document.querySelector('.modalNew-bg');
+      const inputs = Array.from(document.querySelectorAll('input'));
+      Array.from(document.querySelectorAll('.editBtn')).forEach(
+        (btn) => {
+          btn.addEventListener('click', async () => {
+            modalNewBg.classList.add('modal-active');
+            const values = inputs.map(({ value }) => value.trim());
+            
+            // let question = prompt('Are you sure?').trim().toLowerCase();
+          // if (question == 'yes') {
+          // } else {
+          //   alert('Try again');
+          //   setTimeout(function () {
+          //     location.reload();
+          //   }, 500);
+          // }
+            const [name, shortDescription] = values;
+            // let newEdit=document.querySelector('.nameEditInput')
+            // newEdit.setAttribute('placeholder', '');
+            // document.querySelector('shortDescEditInput')
+            //.setAttribute('placeholder', 'Writer the new short descrption')
+          //document.querySelector('#run').setAttribute('class', 'editRun');
+            const response = await fetch(
+              `https://character-database.becode.xyz/characters`,
+              {
+                method: 'GET',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  name,
+                  shortDescription,
+                  //  image,
+                  //description,
+                }),
+              }
+              
+            );
+
+            const freshHero = await response.json();
+          });
+          
+          modalClose.addEventListener('click', () => {
+            modalBg.classList.remove('modal-active');
+          });
+        }
+
+      );
+
+   
+    
+  
+
 
 // new button&modal//
 
@@ -157,7 +175,7 @@
 })();
 
 // to open load the image
-
+})();
 function encodeImageFileAsURL() {
   let filesSelected = document.getElementById('inputFileToLoad').files;
   if (filesSelected.length > 0) {
@@ -177,11 +195,12 @@ function encodeImageFileAsURL() {
       console.log(newSrc);
       //console.log(srcData)
       document.getElementById('imgTest').innerHTML = newImage.innerHTML;
-      alert('Converted Base64 version is '); //+ document.getElementById("imgTest").innerHTML);
+      //alert('Converted Base64 version is '); //+ document.getElementById("imgTest").innerHTML);
       console.log(
         'Converted Base64 version is' +
           document.getElementById('imgTest').innerHTML
       );
+  
 
       // submit the data to JSON
       const inputs = Array.from(document.querySelectorAll('input'));
@@ -192,7 +211,12 @@ function encodeImageFileAsURL() {
           console.error("There's an empty input!");
           return;
         }
-
+// to refresh the window
+          setTimeout(function () {
+              location.reload();
+              }, 500);
+  
+  
         //const [ description, shortDescription, name, image ] = values;
         const [name, shortDescription, image, description] = values;
 
@@ -219,32 +243,33 @@ function encodeImageFileAsURL() {
       });
     };
     fileReader.readAsDataURL(fileToLoad);
+    }
   }
-}
+
 // to refresh the window
-document.getElementById('run').addEventListener('click', () => {
-  //alert("page refresh")
-  setTimeout(function () {
-    location.reload();
-  }, 500);
-});
+// document.getElementById('run').addEventListener('click', () => {
+//   //alert("page refresh")
+//   setTimeout(function () {
+//     location.reload();
+//   }, 500);
+// });
 
 // EDIT to refresh func //
-Array.from(document.querySelectorAll('.editRun')).forEach((btn, index) => {
-  const inputs = Array.from(document.querySelectorAll('input'));
-  btn.addEventListener('click', () => {
-    //alert("page refresh")
-    let question = prompt('Are you sure?').trim().toLowerCase();
-    if (question == 'yes') {
-      confirm(`$Yes`);
-    } else {
-      alert('Try again');
-      setTimeout(function () {
-        location.reload();
-      }, 500);
-    }
-  });
-});
+// Array.from(document.querySelectorAll('.editRun')).forEach((btn, index) => {
+//   const inputs = Array.from(document.querySelectorAll('input'));
+//   btn.addEventListener('click', () => {
+//     //alert("page refresh")
+//     let question = prompt('Are you sure?').trim().toLowerCase();
+//     if (question == 'yes') {
+//       confirm(`$Yes`);
+//     } else {
+//       alert('Try again');
+//       setTimeout(function () {
+//         location.reload();
+//       }, 500);
+//     }
+//   });
+// });
 //   Array.from(document.querySelectorAll('.delBtn')).forEach(
 //     (btn, index) => {
 //       const inputs = Array.from(document.querySelectorAll('input'));
